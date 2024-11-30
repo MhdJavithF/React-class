f6f36acf44e3b65b3d98f3d8e88e18d8cc0c1cc2
MhdJavithF <javithmd001@gmail.com>
2024-11-30 06:52:26 +0530

    Add watchList hover

--- class-4/imdb/src/Components/MovieCard.jsx
+++ class-4/imdb/src/Components/MovieCard.jsx
@@ -23,14 +23,16 @@ const MovieCard = ({movie, onUpdateWatchlist, watchlist}) => {
             <Link to={`/movie-detail/${movie.id}`}><img src={`https://image.tmdb.org/t/p/w500/${movie.poster_path}`}/></Link>
             <Link to={`/movie-detail/${movie.id}`}><h4>{movie.title}</h4></Link>
             <div className="add-watchlist" data-id={movie.id} onClick={handleWatchlistMovie}>
-                <svg class="ipc-watchlist-ribbon__bg" width="24px" height="34px" viewBox="0 0 24 34" xmlns="http://www.w3.org/2000/svg" role="presentation">
+                <div className="watchlist-ribbon">
+                    <svg class="ipc-watchlist-ribbon__bg" width="24px" height="34px" viewBox="0 0 24 34" xmlns="http://www.w3.org/2000/svg" role="presentation">
                     <polygon class="ipc-watchlist-ribbon__bg-ribbon" fill="#000000" points="24 0 0 0 0 32 12.2436611 26.2926049 24 31.7728343"></polygon>
                     <polygon class="ipc-watchlist-ribbon__bg-shadow" points="24 31.7728343 24 33.7728343 12.2436611 28.2926049 0 34 0 32 12.2436611 26.2926049">
                     </polygon>
                 </svg>
                 <div id="icon">{isMovieAdded ? "--": "+"}</div>
+                </div>
+                <span>Add Watchlist</span>
             </div>
-            
         </div>
     )
 }
