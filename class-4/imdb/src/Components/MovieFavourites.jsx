e9179dbd79fb704ddd49bb043e6c877833bea92c
MhdJavithF <javithmd001@gmail.com>
2024-12-02 00:23:38 +0530

    sorting, filtering, searching and removing functionalities in fav page

--- class-4/imdb/src/Components/MovieFavourites.jsx
+++ class-4/imdb/src/Components/MovieFavourites.jsx
@@ -1,4 +1,5 @@
 import { useEffect, useState } from "react";
+import { FaArrowUpLong,FaArrowDownLong } from "react-icons/fa6";
 import Heading from "./Heading";
 
 const MovieFavourites = () => {
@@ -47,10 +48,48 @@ const MovieFavourites = () => {
             return favMovieList.filter(movie => !selectGenre || movie.genre_ids[0] == selectGenre)
         });
     },[selectGenre,favMovieList])
+
+    const handleSearch = (e) => {
+        const search = e.target.value;
+        setFilterMovieList(()=>{
+            return favMovieList.filter(movie => movie.title.toLowerCase().includes(search.toLowerCase()));
+        })
+    }
+
+    const handleSort = (e) => {
+        const sortingType = e.currentTarget.dataset.type;
+        setFilterMovieList(()=>{
+            if(!sortingType && !selectGenre){
+                return favMovieList;
+            }
+            else if(!sortingType){
+                return filterMovieList;
+            }
+            else{
+                return [...filterMovieList].sort((a,b) => sortingType == "ASC" ? a.popularity - b.popularity : b.popularity - a.popularity);
+            }
+        });
+    }
+
+    const handleRemoveMovie = (movieId) => () => {
+        setFavMovieList((prevFavList) => {
+            const movieIdx = prevFavList.findIndex(movie => movie.id == movieId);
+            const finalFavList = [...prevFavList];
+            finalFavList.splice(movieIdx,1);
+            localStorage.setItem("favourites", JSON.stringify(finalFavList));
+            return finalFavList;
+        })
+        
+    }
+
     return (
         <div>
             <Heading watchlistCount={favMovieList.length}/>
-            <h2 id="fav-head">Favourite Movies</h2>
+            <div className="fav-head">
+                <h2> Favourite Movies</h2>
+                <input type="text" placeholder="Search by title..." onChange={handleSearch}/>
+            </div>
+            
             <div className="fav-wrapper">
                 <div className="left-sec">
                     <div className="genres-wrapper">
@@ -69,28 +108,31 @@ const MovieFavourites = () => {
                                 <th>Image</th>
                                 <th>Title</th>
                                 <th>Genre</th>
-                                <th>Popularity</th>
+                                <th>
+                                    <span data-type="" onClick={handleSort}>Popularity</span>
+                                    <span data-type="DEC" onClick={handleSort}><FaArrowUpLong/></span>
+                                    <span data-type="ASC" onClick={handleSort}><FaArrowDownLong/></span>
+                                </th>
                                 <th>Rating</th>
                                 <th>Acions</th>
                             </tr>
                         </thead>
                         <tbody>
                                 {
-                                    filterMovieList?.map((fav) => (
+                                    filterMovieList.length == 0 ? <p>Please add some Favourite movie!</p> :filterMovieList?.map((fav) => (
                                         <tr>
                                             <td><img src={`https://image.tmdb.org/t/p/w500/${fav.poster_path}`} style={{width:"120px"}}/></td>
                                             <td>{fav.title}</td>
                                             <td>{genreids[fav.genre_ids[0]]}</td>
                                             <td>{fav.popularity}</td>
                                             <td>{fav.vote_average}</td>
-                                            <td><button>Remove</button></td>
+                                            <td><button onClick={handleRemoveMovie(fav.id)}>Remove</button></td>
                                         </tr>
                                     ))
                                 }
                         </tbody>
                     </table>
                 </div>
-                
             </div>
         </div>
     )
